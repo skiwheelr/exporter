1da967c54678212a893d963be020070705e503c8
Terrapin <Terrapin@Terrapins-MBP.Home>
2018-01-31 15:12:02 -0500

    2pages

---
+++ src/exportasst/Main.java
@@ -0,0 +1,14 @@
+package exportasst;
+import edu.duke.FileResource;
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class Main {
+    
+  public static void main(String[] args){
+       Main lister = new Main();
+     //  Exportasst.listExporters();
+       System.out.println("Mainrunning");
+    }
+
+}
--- src/exportasst/exportasst.java
+++ src/exportasst/exportasst.java
@@ -1,19 +1,19 @@
-import edu.duke.*;
-import org.apache.commons.csv.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exportasst;
 
-package Exportasst;
+import edu.duke.FileResource;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
-
-
-public class Exportasst {
-
-    public void Exportasst(String[] args){
-        Exportasst lister = new Exportasst();
-        lister.whoExportsCoffee();
-        System.out.println("Mainrunning");
-    }
-
-
+/**
+ *
+ * @author Terrapin
+ */
+public class exportasst {
 public void listExporters(CSVParser parser, String exportOfInterest) {
         //for each row in the CSV File
         for (CSVRecord record : parser) {
@@ -29,9 +29,10 @@ public void listExporters(CSVParser parser, String exportOfInterest) {
     }
 
     public void whoExportsCoffee() {
-        FileResource fr = new FileResource();
+        FileResource fr = new FileResource("./exportdata.csv");
         CSVParser parser = fr.getCSVParser();
         listExporters(parser, "coffee");
+        
     }
 
 }
